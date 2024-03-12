6d5bc58897d90a7005ea5ed7f2f02b286b76e5bb
ZomB37 <HowlingNights@gmail.com>
2024-03-12 02:21:54 -0700

    setting up gam variables

---
+++ BSLogic.js
@@ -0,0 +1,18 @@
+//BASE GAME VARIABLES
+
+//create 3 variables to hold ship location data
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
+
+//variable to track user input
+var userGuess = 0;
+
+//variable to track the number of successful hits 
+var numOfHits = 0;
+
+//variable to track the total number of user guesses
+var totalGuesses = 0;
+
+//variable to track whether the ship has been sunk
+var isSunk = false;
